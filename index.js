12f1d4d7cd653f324e13656f878de95ce7f34029
Hidenori Fujimura <hfu@users.noreply.github.com>
2018-02-02 18:28:55 +0000

    update

---
+++ index.js
@@ -0,0 +1,9 @@
+exports.scaffold = {
+  version: 8,
+  center: [35, 135],
+  zoom: 5,
+  sources: {},
+  sprite: "",
+  glyphs: "https://hfu.github.io/openmaptiles-fonts/{fontstack}/{range}.pbf",
+  layers: []
+}
